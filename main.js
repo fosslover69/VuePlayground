7ec3055e3e0d17dab4ed5019650566889efcdb32
Tamizh Arasan <no.1opensourcelover@protonmail.com>
2022-05-18 19:22:55 +0530

    L2: Add Description.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            Product: "Programming Socks.",
+            Description: "Using this Socks will make you Greatest Programmer of all Time."
+        }
+    }
+})
