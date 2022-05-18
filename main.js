5715a64ffe58bbc4f7a7727f3330e3a01090017f
Tamizh Arasan <no.1opensourcelover@protonmail.com>
2022-05-18 22:24:50 +0530

    L5: Add Varients and Sizes

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            Sizes: [69,70,71,42],
+            Varients: [
+                {id: 69, Color: "Blue"},
+                {id: 420, Color: "Red"}
+            ]
         }
     }
 })
